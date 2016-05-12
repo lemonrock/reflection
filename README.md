e2131d2e2df00f89b45818a428ecb5b35c47dc17
Raphael Cohn <raphael.cohn@stormmq.com>
2016-05-12 08:17:11 +0100

    Working build

---
+++ README.md
@@ -0,0 +1,13 @@
+[](This file is part of reflection. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/reflection/master/COPYRIGHT. No part of reflection, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.)
+[](Copyright © 2016 The developers of reflection. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/reflection/master/COPYRIGHT.)
+
+# reflection
+
+[reflection] is a rust crate.
+
+
+## Licensing
+
+The license for this project is MIT.
+
+[reflection]: https://github.com/lemonrock/reflection "reflection GitHub page"
---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+// This file is part of rust1. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/rust1/master/COPYRIGHT. No part of rust1, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+// Copyright © 2016 The developers of rust1. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/rust1/master/COPYRIGHT.
+
+#![feature(core_intrinsics)]
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+pub fn typeOf<T>(_: &T) -> &str
+{
+	unsafe
+	{
+		&(std::intrinsics::type_name::<T>())
+	}
+}
+
+pub trait CanonicalName
+{
+    fn canonicalName(&self) -> &str
+	{
+		unsafe
+		{
+		    // We are allowed to return a reference (&str) here because type_name() returns a 'static str;
+		    // if it returned just str, then we'd have a free-before-use problem
+			&(*std::intrinsics::type_name::<Self>())
+		}
+	}
+}
+
+impl<T> CanonicalName for T
+{
+}
